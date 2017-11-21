0c1c74c78bafaebe606624d61b587cbe9df5b0b3
xuhui <1429420198@qq.com>
2017-11-21 18:08:06 +0800

    main.js 引入 element-ui 样式注释掉 到时候自己加上

--- logistic-project/src/main.js
+++ logistic-project/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import "babel-polyfill";//可以使用es6转化成es5 而不用担心有些浏览器不兼容
-//import 'element-ui/lib/theme-default/index.css';    // 引入对应的默认主题 element-ui的版本不一样引入的路径也不一样
+// 引入对应的默认主题 element-ui的版本不一样引入的路径也不一样
 
 
 Vue.config.productionTip = false
