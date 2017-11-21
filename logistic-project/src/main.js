d2bc668e3f7fa231984465188ee02970c53b6c77
xuhui <1429420198@qq.com>
2017-11-21 17:31:31 +0800

    main.js根据不同的element-ui版本自行修改

--- logistic-project/src/main.js
+++ logistic-project/src/main.js
@@ -6,7 +6,8 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import "babel-polyfill";//可以使用es6转化成es5 而不用担心有些浏览器不兼容
-import 'element-ui/lib/theme-default/index.css';    // 默认主题
+//import 'element-ui/lib/theme-default/index.css';    // 引入对应的默认主题 element-ui的版本不一样引入的路径也不一样
+
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
